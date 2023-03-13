179ef6d19be0f870b541615ab1e4e7ab72219ab5
felixfabri <felix.fabrijr@gmail.com>
2023-03-13 10:57:10 +0000

    Add project files.

---
+++ Clists/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Clists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nomes = new List<string> { "<nome>", "Ana", "Felipe" };
+
+            foreach (var nome in nomes)
+            {
+                Console.WriteLine($"Ola {nome.ToUpper()}!");
+            }
+        }
+    }
+}
